639c1692ed3cb9b7056cba0e93a1a5ef40166515
SunXingZ <sunjian.shanghai@foxmail.com>
2020-07-28 16:27:41 +0800

    增加条码打印

--- utils/tsc.js
+++ utils/tsc.js
@@ -2,6 +2,8 @@ import * as Util from "./utils";
 
 const _ = require('lodash');
 
+// https://www.docin.com/p-248730840.html 指令文档
+
 let writeTextToDevice;
 
 function _setBT(bt) {
@@ -9,12 +11,12 @@ function _setBT(bt) {
 }
 
 let Config = {
-  width: 60,
-  height: 40,
-  gap: 2,
+  width: 70,
+  height: 60,
+  gap: 0,
   direction: 0,
-  referenceX: 30,
-  referenceY: 30
+  referenceX: 10,
+  referenceY: 10
 };
 
 const TSC = {
@@ -22,18 +24,39 @@ const TSC = {
   config(con) {
     Object.assign(Config, con);
   },
+  /**
+   * 设置卷标纸的宽度及长度
+   * @param {number} width 
+   * @param {number} height 
+   */
   size(width, height) {
     writeTextToDevice(`SIZE ${width} mm, ${height} mm\r\n`);
   },
+  /**
+   * 设置两张卷标纸间的垂直间距距离
+   * @param {number} g 
+   */
   gap(g) {
     writeTextToDevice(`GAP ${g} mm\r\n`);
   },
+  /**
+   * 设置打印的出纸方向
+   * @param {number} dir 0或1
+   */
   direction(dir = 0) {
     writeTextToDevice(`DIRECTION ${dir}\r\n`);
   },
+  /**
+   * 设置卷标的参考坐标原点
+   * @param {number} x 
+   * @param {number} y 
+   */
   reference(x = 0, y = 0) {
     writeTextToDevice(`REFERENCE ${x},${y}\r\n`);
   },
+  /**
+   * 清除影像缓冲区的数据
+   */
   cls() {
     writeTextToDevice(`CLS \r\n`);
   },
@@ -47,7 +70,7 @@ const TSC = {
   },
 
   text(x, y, text, x_times = 1, y_times = 1) {
-    writeTextToDevice(`TEXT ${x},${y},"TSS24.BF2", 0,${x_times},${y_times},"${text}"`)
+    writeTextToDevice(`TEXT ${x},${y},"TSS24.BF2",0,${x_times},${y_times},"${text}"\r\n`)
   },
   print(times = 1) {
     writeTextToDevice(`PRINT ${times}\r\n`);
@@ -60,6 +83,9 @@ const TSC = {
   },
   drawBar(x, y, width, height) {
     writeTextToDevice(`BAR ${x},${y},${width},${height}\r\n`);
+  },
+  drawBarCode(x, y, height, barcode_content) {
+    writeTextToDevice(`BARCODE ${x},${y},"128",${height},1,0,3,3,"${barcode_content}"\r\n`)
   },
   drawQrcode(x, y, ecc_level, cell_width, mode, rotation, qrcode_content) {
     writeTextToDevice(`QRCODE ${x},${y},${ecc_level},${cell_width},${mode},${rotation},"${qrcode_content}"\r\n`)
